7bdd1fae7f350946e3137fd55c6dd2c66642269f
Xenan <k.kirill@meta.studio>
2024-11-13 11:32:11 +0700

    feat: add SitemapItemDTO

---
+++ src/DTO/SitemapItemDTO.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DTO;
+
+readonly class SitemapItemDTO
+{
+    public function __construct(
+        public string $loc,
+        public string $lastmod,
+        public float $priority,
+        public string $changefreq
+    ) {}
+
+    /**
+     * @return array<self>
+     */
+    public static function fromPages(array $pages): array
+    {
+        return array_map(function (array $page) {
+            return self::fromPage($page);
+        }, $pages);
+    }
+
+    public static function fromPage(array $page): self
+    {
+        return new self($page['loc'], $page['lastmod'], $page['priority'], $page['changefreq']);
+    }
+}
