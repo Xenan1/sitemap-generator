023fa92b6ba6e053c1007d08b2b7e4c1bbeef5b5
Xenan <k.kirill@meta.studio>
2024-11-13 14:17:52 +0700

    fix: xml generation

--- src/FileGenerators/XmlGenerator.php
+++ src/FileGenerators/XmlGenerator.php
@@ -11,8 +11,7 @@ class XmlGenerator extends AbstractFileGenerator
 
     protected function prepare(): void
     {
-        $this->xml = new SimpleXMLElement('<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">');
-
+        $this->xml = new SimpleXMLElement('<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"></urlset>');
     }
 
     protected function addSitemapItem(SitemapItemDTO $item): void
