b148b1dbf1963af4f62b84ab0871576d64a56e23
Xenan <k.kirill@meta.studio>
2024-11-13 11:32:01 +0700

    feat: add XmlFileGenerator

---
+++ src/FileGenerators/XmlGenerator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FileGenerators;
+
+use DTO\SitemapItemDTO;
+use SimpleXMLElement;
+
+class XmlGenerator extends AbstractFileGenerator
+{
+    private SimpleXMLElement $xml;
+
+    protected function prepare(): void
+    {
+        $this->xml = new SimpleXMLElement('<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">');
+
+    }
+
+    protected function addSitemapItem(SitemapItemDTO $item): void
+    {
+        $xmlItem = $this->xml->addChild('url');
+        $xmlItem->addChild('loc', $item->loc);
+        $xmlItem->addChild('lastmod', $item->lastmod);
+        $xmlItem->addChild('priority', $item->priority);
+        $xmlItem->addChild('changefreq', $item->changefreq);
+    }
+
+    protected function saveFile(): void
+    {
+        $this->xml->asXML($this->getDestinationPath());
+    }
+}
