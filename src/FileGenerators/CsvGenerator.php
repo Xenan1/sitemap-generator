54cd2b915c32f003bc1985f319ed0c0100f0bbc9
Xenan <k.kirill@meta.studio>
2024-11-13 11:31:44 +0700

    feat: add CsvFileGenerator

---
+++ src/FileGenerators/CsvGenerator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace FileGenerators;
+
+use DTO\SitemapItemDTO;
+
+class CsvGenerator extends AbstractFileGenerator
+{
+    private array $data = [];
+
+    protected function prepare(): void
+    {
+        $this->data[] = ['loc', 'lastmod', 'priority', 'changefreq'];
+    }
+
+    protected function addSitemapItem(SitemapItemDTO $item): void
+    {
+        $this->data[] = [
+            $item->loc,
+            $item->lastmod,
+            $item->priority,
+            $item->changefreq,
+        ];
+    }
+
+    protected function saveFile(): void
+    {
+        $file = fopen($this->getDestinationPath(), 'w');
+
+        foreach ($this->data as $csvRow) {
+            fputcsv($file, $csvRow);
+        }
+
+        fclose($file);
+    }
+}
