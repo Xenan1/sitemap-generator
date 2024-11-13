6a39c8afcc933acc714b275a584b924c3f3e12cd
Xenan <k.kirill@meta.studio>
2024-11-13 10:45:16 +0700

    feat: add FilePathValidator

---
+++ src/Validators/FilePathValidator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Validators;
+
+use Enums\FileType;
+use ValueError;
+
+class FilePathValidator
+{
+    public function __construct(private string $path) {}
+
+    /**
+     * @throws ValueError
+     */
+    public static function validateForFileWriting(string $path, FileType $expectedFileType): void
+    {
+        $validator = new static($path);
+        $validator->validateWritingPermissions();
+        $validator->validateFileExtension($expectedFileType);
+    }
+
+    private function validateWritingPermissions(): void
+    {
+        if (!is_writable($this->path)) {
+            throw new ValueError('Destination path is not writable');
+        }
+    }
+
+    public static function validatePathString($path): void
+    {
+        if (!preg_match('/^[^*?"<>|:]*$/', $path)) {
+            throw new ValueError('Invalid file path');
+        }
+    }
+
+    private function validateFileExtension(FileType $expectedFileType): void
+    {
+        $pathParts = explode('.', $this->path);
+        $extension = end($pathParts);
+
+        $expectedExtension = $expectedFileType->extension();
+        if ($expectedExtension !== $extension) {
+            throw new ValueError('Unexpected file extension. Expected '. $expectedExtension);
+        }
+    }
+}
