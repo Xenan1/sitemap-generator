3c68048739866e089d875ced51d6ca891612e3c1
Xenan <k.kirill@meta.studio>
2024-11-13 11:31:34 +0700

    feat: add AbstractFileGenerator

---
+++ src/FileGenerators/AbstractFileGenerator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace FileGenerators;
+
+use DTO\SitemapItemDTO;
+
+abstract class AbstractFileGenerator
+{
+    private string $destinationPath;
+    /**
+     * @param array<SitemapItemDTO> $pages
+     * @param string $destinationPath
+     * @return void
+     */
+
+    abstract protected function prepare(): void;
+    abstract protected function addSitemapItem(SitemapItemDTO $item): void;
+    abstract protected function saveFile(): void;
+
+    public function run(array $pages, string $destinationPath): void
+    {
+        $this->destinationPath = $destinationPath;
+
+        $this->prepare();
+        foreach ($pages as $page) {
+            $this->addSitemapItem($page);
+        }
+        $this->saveFile();
+    }
+
+    protected function getDestinationPath(): string
+    {
+        return $this->destinationPath;
+    }
+}
