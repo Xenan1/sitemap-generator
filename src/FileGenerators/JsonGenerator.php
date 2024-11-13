e5276d206237f5aa65dcbd3259f2cf2f27de8f40
Xenan <k.kirill@meta.studio>
2024-11-13 11:31:51 +0700

    feat: add JsonFileGenerator

---
+++ src/FileGenerators/JsonGenerator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace FileGenerators;
+
+use DTO\SitemapItemDTO;
+
+class JsonGenerator extends AbstractFileGenerator
+{
+    private array $data;
+
+    protected function prepare(): void
+    {
+        $this->data = [];
+    }
+
+    protected function addSitemapItem(SitemapItemDTO $item): void
+    {
+        $this->data[] = [
+            'loc' => $item->loc,
+            'lastmod' => $item->lastmod,
+            'priority' => $item->priority,
+            'changefreq' => $item->changefreq,
+        ];
+    }
+
+    protected function saveFile(): void
+    {
+        $content = json_encode($this->data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+        file_put_contents($this->getDestinationPath(), $content);
+    }
+}
